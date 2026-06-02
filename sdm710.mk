6e37f792fa8336ac04dab927706ab0f0ac6f4d18
sashascurtu <sasha.scurtu@gmail.com>
2026-06-02 22:58:14 +0300

    sdm710-common: Build sensorservice 1.0 HAL

--- sdm710.mk
+++ sdm710.mk
@@ -287,7 +287,9 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-service \
+    android.frameworks.sensorservice@1.0.vendor \
+    android.frameworks.sensorservice@1.0
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
